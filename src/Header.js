15d84b126c9bd5050558e2b964a114d44a92d495
chrystian cavalcante <chrystiancavalcante@gmail.com>
2018-04-06 20:41:43 -0300

    first commit

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     <h1 className='site-heading text-center text-white d-none d-lg-block'>
         {/*<span className="site-heading-upper text-primary mb-3">Um super asilo</span>
       <span className="site-heading-lower">Nossa Senhora de Fátima</span>*/}
-        <img src='img/logo.png' />
+        <img src='img/logo.png' alt='logo' />
     </h1>
 
     {/*  Navigation*/}
